9824912e8f93433b56fd0197eb7331d4e896a72f
surajthomask <suthomas@altayer.com>
2018-07-16 15:58:48 +0400

    Initial Commit
    Change-Id: I3b4cca69775cd861416d6734fdb5be887d8efa63

---
+++ ATGMediaBrowser/ATGMediaBrowser.h
@@ -0,0 +1,22 @@
+//
+//  ATGMediaBrowser.h
+//  ATGMediaBrowser
+//
+//  Created by Suraj Thomas K on 7/16/18.
+//  Copyright © 2018 Al Tayer Group LLC. All rights reserved.
+//
+//  Save to the extent permitted by law, you may not use, copy, modify,
+//  distribute or create derivative works of this material or any part
+//  of it without the prior written consent of Al Tayer Group LLC.
+//  Any reproduction of this material must contain this notice.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATGMediaBrowser.
+FOUNDATION_EXPORT double ATGMediaBrowserVersionNumber;
+
+//! Project version string for ATGMediaBrowser.
+FOUNDATION_EXPORT const unsigned char ATGMediaBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATGMediaBrowser/PublicHeader.h>
