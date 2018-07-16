2d75da22baffbbd63da64c91a6082568c7b07788
surajthomask <suthomas@altayer.com>
2018-07-16 13:31:02 +0400

    Add horizontal slide out transition
    Change-Id: I4922ec2818b3b0f04920dce6f39e4885df5690a7

--- ATGMediaBrowser/MediaBrowser/ContentTransformers.swift
+++ ATGMediaBrowser/MediaBrowser/ContentTransformers.swift
@@ -17,15 +17,41 @@ public typealias ContentTransformer = (UIView, CGFloat) -> Void
 
 public enum DefaultContentTransformers {
 
+    // GestureDirection : Horizontal
+    // DrawOrder : Any
     public static let horizontalMoveInOut: ContentTransformer = { contentView, position in
 
-        let widthIncludingGap = contentView.frame.size.width + MediaContentView.interItemSpacing
+        let widthIncludingGap = contentView.bounds.size.width + MediaContentView.interItemSpacing
         contentView.transform = CGAffineTransform(translationX: widthIncludingGap * position, y: 0.0)
     }
 
+    // GestureDirection : Vertical
+    // DrawOrder : Any
     public static let verticalMoveInOut: ContentTransformer = { contentView, position in
 
-        let heightIncludingGap = contentView.frame.size.height + MediaContentView.interItemSpacing
+        let heightIncludingGap = contentView.bounds.size.height + MediaContentView.interItemSpacing
         contentView.transform = CGAffineTransform(translationX: 0.0, y: heightIncludingGap * position)
+    }
+
+    // GestureDirection : Horizontal
+    // DrawOrder : PreviousToNext
+    public static let horizontalSlideOut: ContentTransformer = { contentView, position in
+
+        var scale: CGFloat = 1.0
+        if position < -0.5 {
+            scale = 0.9
+        } else if -0.5...0.0 ~= Double(position) {
+            scale = 1.0 + (position * 0.2)
+        }
+        var transform = CGAffineTransform(scaleX: scale, y: scale)
+
+        let widthIncludingGap = contentView.bounds.size.width + MediaContentView.interItemSpacing
+        let x = position >= 0.0 ? widthIncludingGap * position : 0.0
+        transform = transform.translatedBy(x: x, y: 0.0)
+
+        contentView.transform = transform
+
+        let margin: CGFloat = 0.0000001
+        contentView.isHidden = ((1.0-margin)...(1.0+margin) ~= abs(position))
     }
 }
