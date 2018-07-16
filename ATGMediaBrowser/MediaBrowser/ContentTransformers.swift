a3371969e59525222b81c79cd2f84647a0bd31cc
surajthomask <suthomas@altayer.com>
2018-07-16 13:32:06 +0400

    Add horizontalSlideIn and verticalSlideIn transitions
    Change-Id: Iad42e5a236a36e4a32a9277c7d47358f89a29872

--- ATGMediaBrowser/MediaBrowser/ContentTransformers.swift
+++ ATGMediaBrowser/MediaBrowser/ContentTransformers.swift
@@ -76,4 +76,48 @@ public enum DefaultContentTransformers {
         let margin: CGFloat = 0.0000001
         contentView.isHidden = ((1.0-margin)...(1.0+margin) ~= abs(position))
     }
+
+    // GestureDirection : Horizontal
+    // DrawOrder : NextToPrevious
+    public static let horizontalSildeIn: ContentTransformer = { contentView, position in
+
+        var scale: CGFloat = 1.0
+        if position > 0.5 {
+            scale = 0.9
+        } else if 0.0...0.5 ~= Double(position) {
+            scale = 1.0 - (position * 0.2)
+        }
+        var transform = CGAffineTransform(scaleX: scale, y: scale)
+
+        let widthIncludingGap = contentView.bounds.size.width + MediaContentView.interItemSpacing
+        let x = position > 0.0 ? 0.0 : widthIncludingGap * position
+        transform = transform.translatedBy(x: x, y: 0.0)
+
+        contentView.transform = transform
+
+        let margin: CGFloat = 0.0000001
+        contentView.isHidden = ((1.0-margin)...(1.0+margin) ~= abs(position))
+    }
+
+    // GestureDirection : Vertical
+    // DrawOrder : NextToPrevious
+    public static let verticalSlideIn: ContentTransformer = { contentView, position in
+
+        var scale: CGFloat = 1.0
+        if position > 0.5 {
+            scale = 0.9
+        } else if 0.0...0.5 ~= Double(position) {
+            scale = 1.0 - (position * 0.2)
+        }
+        var transform = CGAffineTransform(scaleX: scale, y: scale)
+
+        let heightIncludingGap = contentView.bounds.size.height + MediaContentView.interItemSpacing
+        let y = position > 0.0 ? 0.0 : heightIncludingGap * position
+        transform = transform.translatedBy(x: 0.0, y: y)
+
+        contentView.transform = transform
+
+        let margin: CGFloat = 0.0000001
+        contentView.isHidden = ((1.0-margin)...(1.0+margin) ~= abs(position))
+    }
 }
